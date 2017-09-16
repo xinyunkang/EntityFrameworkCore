336a4711b1e15d32bbb4a1b31a6281a67c65429f
xinyunkang <xinyunkang@gmail.com>
2017-09-16 11:16:09 -0500

    Raw sql

--- SamuraiApp/SomeUI/Program.cs
+++ SamuraiApp/SomeUI/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Logging;
 using SamuraiApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace SomeUI
 {
@@ -23,7 +26,96 @@ namespace SomeUI
             //MoreQueries();
             //RetrieveAndUpdateSamurai();
             //RetrieveAndUpdateMultipleSamurais();
-            QueryAndUpdateSamuraiDisconnected();
+            //QueryAndUpdateSamuraiDisconnected(); 
+            //DeleteWhileTracked();                                  
+            //DeleteWhileNotTracked();                                     
+            // DeleteMany();
+            //RawSqlQuery();
+            //RawSqlStoredProcedure();
+            RawSqlCommand();
+        }
+
+
+
+        private static void RawSqlCommand()
+        {
+            var affected = _context.Database.ExecuteSqlCommand(
+              "update samurais set Name=REPLACE(Name,'Wu','Amber')");  //return the number of rows affected.
+            Console.WriteLine($"Affected rows {affected}");
+        }
+
+
+
+        private static void RawSqlStoredProcedure()
+        {
+            var namePart = "Kevin";
+            var samurais = _context.Samurais
+              .FromSql("EXEC FilterSamuraiByNamePart {0}", namePart)
+              .OrderByDescending(s => s.Name).ToList();  //does not add to the stored procedure code. processed in memory.
+
+            samurais.ForEach(s => Console.WriteLine(s.Name));
+            Console.WriteLine();
+        }
+
+        private static void RawSqlQuery()
+        {
+            var samurais = _context.Samurais.FromSql("Select * from Samurais")
+                          .OrderByDescending(s => s.Name)   //will add to the query directly
+                          .Where(s => s.Name.Contains("Kang")).ToList();  //will add to the query directly
+            
+            samurais.ForEach(s => Console.WriteLine(s.Name));
+            Console.WriteLine();
+        }
+
+
+        
+
+
+        private static void RawSqlCommandWithOutput()
+        {
+            var procResult = new SqlParameter
+            {
+                ParameterName = "@procResult",
+                SqlDbType = SqlDbType.VarChar,
+                Direction = ParameterDirection.Output,
+                Size = 50
+            };
+            _context.Database.ExecuteSqlCommand(
+              "exec FindLongestName @procResult OUT", procResult);
+            Console.WriteLine($"Longest name: {procResult.Value}");
+        }
+
+
+
+
+        private static void DeleteWhileTracked()
+        {
+            var samurai = _context.Samurais.FirstOrDefault(s => s.Name == "Kambei Shimada");
+            _context.Samurais.Remove(samurai);
+            //alternates:
+            // _context.Remove(samurai);
+            // _context.Entry(samurai).State=EntityState.Deleted;
+            // _context.Samurais.Remove(_context.Samurais.Find(1));
+            _context.SaveChanges();
+        }
+
+        private static void DeleteMany()
+        {
+            var samurais = _context.Samurais.Where(s => s.Name.Contains("ō"));
+            _context.Samurais.RemoveRange(samurais);
+            //alternate: _context.RemoveRange(samurais);
+            _context.SaveChanges();
+        }
+
+        private static void DeleteWhileNotTracked()
+        {
+            var samurai = _context.Samurais.FirstOrDefault(s => s.Name == "Heihachi Hayashida");
+            using (var contextNewAppInstance = new SamuraiContext())
+            {
+                contextNewAppInstance.Samurais.Remove(samurai);
+                //contextNewAppInstance.Entry(samurai).State=EntityState.Deleted;
+                contextNewAppInstance.SaveChanges();
+            }
 
         }
 
